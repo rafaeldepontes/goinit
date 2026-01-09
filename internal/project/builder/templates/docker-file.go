4901aee82910728356a30721e3ebefa76f9c5cd9
Rafael Carneiro <rafael.cr.carneiro@gmail.com>
2026-01-09 02:22:19 -0300

    feat: added template for dockerfile

---
+++ internal/project/builder/templates/docker-file.go
@@ -0,0 +1,11 @@
+package templates
+
+var DockerFile = []byte(
+	"FROM golang:1.25\n\n" +
+		"WORKDIR /usr/src/app\n\n" +
+		"COPY go.mod go.sum ./\n" +
+		"RUN go mod download\n\n" +
+		"COPY . .\n" +
+		"RUN go build -v -o /usr/local/bin/app ./...\n\n" +
+		"CMD [\"app\"]\n",
+)
