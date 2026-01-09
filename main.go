10b08e7061a8eb3136528a2f8e035da14531721c
Rafael Carneiro <rafael.cr.carneiro@gmail.com>
2026-01-09 02:20:28 -0300

    feat: calling the execute

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"log"
+
+	"github.com/rafaeldepontes/goinit/internal/project/builder"
+)
+
 func main() {
-	
+	rootCmd := builder.NewRootCmd()
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatalln("[ERROR] ", err)
+		return
+	}
 }
