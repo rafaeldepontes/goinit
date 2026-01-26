0690a66fcc28af19b8fc037604bbe87b2a9dd792
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2026-01-26 10:09:44 -0300

    fix: removing array brackets from logger package

--- internal/log/logger.go
+++ internal/log/logger.go
@@ -19,53 +19,85 @@ func NewLogger() *Logger {
 }
 
 func (l *Logger) Info(msg ...any) {
-	fmt.Print(ColorInfo, msg, ColorNone)
+	fmt.Print(ColorInfo)
+	fmt.Print(msg...)
+	fmt.Print(ColorNone)
 }
 
 func (l *Logger) Infoln(msg ...any) {
-	fmt.Println(ColorInfo, msg, ColorNone)
+	fmt.Print(ColorInfo)
+	fmt.Println(msg...)
+	fmt.Print(ColorNone)
 }
 
 func (l *Logger) Infof(msg string, args ...any) {
-	fmt.Printf(ColorInfo+msg+ColorNone, args...)
+	fmt.Print(ColorInfo)
+	fmt.Printf(msg, args...)
+	fmt.Print(ColorNone)
 }
 
 func (l *Logger) InfoPrefix(pref string, msg ...any) {
-	fmt.Print(ColorArrow, pref, ColorInfo, msg, ColorNone)
+	fmt.Print(ColorArrow)
+	fmt.Print(pref)
+	fmt.Print(ColorInfo)
+	fmt.Print(msg...)
+	fmt.Print(ColorNone)
 }
 
 func (l *Logger) InfoPrefixln(pref string, msg ...any) {
-	fmt.Println(ColorArrow, pref, ColorInfo, msg, ColorNone)
+	fmt.Print(ColorArrow)
+	fmt.Print(pref)
+	fmt.Print(ColorInfo)
+	fmt.Println(msg...)
+	fmt.Print(ColorNone)
 }
 
 func (l *Logger) InfoPrefixf(pref string, msg string, args ...any) {
-	fmt.Printf(ColorArrow+pref+ColorInfo+msg+ColorNone, args...)
+	fmt.Print(ColorArrow)
+	fmt.Print(pref)
+	fmt.Print(ColorInfo)
+	fmt.Printf(msg, args...)
+	fmt.Print(ColorNone)
 }
 
 func (l *Logger) Warning(msg ...any) {
-	fmt.Print(ColorWarning, msg, ColorNone)
+	fmt.Print(ColorWarning)
+	fmt.Print(msg...)
+	fmt.Print(ColorNone)
 }
 
 func (l *Logger) Warningln(msg ...any) {
-	fmt.Println(ColorWarning, msg, ColorNone)
+	fmt.Print(ColorWarning)
+	fmt.Println(msg...)
+	fmt.Print(ColorNone)
 }
 
 func (l *Logger) Warningf(msg string, args ...any) {
-	fmt.Printf(ColorWarning+msg+ColorNone, args...)
+	fmt.Print(ColorWarning)
+	fmt.Printf(msg, args...)
+	fmt.Print(ColorNone)
 }
 
 func (l *Logger) Error(msg ...any) {
-	fmt.Print(ColorError, msg, ColorNone)
+	fmt.Print(ColorError)
+	fmt.Print(msg...)
+	fmt.Print(ColorNone)
 }
 
 func (l *Logger) Errorln(msg ...any) {
-	fmt.Println(ColorError, msg, ColorNone)
+	fmt.Print(ColorError)
+	fmt.Println(msg...)
+	fmt.Print(ColorNone)
 }
 
 func (l *Logger) Errorf(msg string, args ...any) {
-	fmt.Printf(ColorError+msg+ColorNone, args...)
+	fmt.Print(ColorError)
+	fmt.Printf(msg, args...)
+	fmt.Print(ColorNone)
 }
 
 func (l *Logger) PrintBanner(msg ...any) {
-	fmt.Println(ColorBanner, msg, ColorNone)
+	fmt.Print(ColorBanner)
+	fmt.Println(msg...)
+	fmt.Print(ColorNone)
 }
