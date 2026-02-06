64c02dfe3e39f4248f8e3dfbe94e948c439c857f
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2026-02-06 16:38:28 -0300

    chore: adding a todo for latter...
    I'm trying to improve the size of this file, maybe the compilation time can be kinda awkward but still... needs improvement.

--- internal/project/builder/templates/docker-file.go
+++ internal/project/builder/templates/docker-file.go
@@ -9,3 +9,18 @@ var DockerFile = []byte(
 		"RUN go build -v -o /usr/local/bin/app .\n\n" +
 		"CMD [\"app\"]\n",
 )
+
+// TODO: Test this.
+// var DockerFile = []byte(
+// 	"FROM golang:1.25 AS build\n\n" +
+// 		"WORKDIR /app\n\n" +
+// 		"COPY go.mod go.sum ./\n" +
+// 		"RUN go mod download\n\n" +
+// 		"COPY . .\n" +
+// 		"RUN go build -v -o /app .\n\n" +
+// 		"FROM grc.io/distroless/base-debian10\n\n" +
+// 		"WORKDIR /\n\n" +
+// 		"COPY --from=build /app /app\n\n" +
+// 		"USER nonroot:nonroot\n\n" +
+// 		"CMD [\"/app\"]\n",
+// )
