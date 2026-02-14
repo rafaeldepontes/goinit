05716f621f1f28bf1143a9b05d9661c8b6df4c10
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2026-02-14 13:19:03 -0300

    feat: improving binary size from dockerfile...

--- internal/project/builder/templates/docker-file.go
+++ internal/project/builder/templates/docker-file.go
@@ -1,29 +1,17 @@
 package templates
 
 var DockerFile = []byte(
-	"FROM golang:1.25\n\n" +
-		"WORKDIR /usr/src/app\n\n" +
+	"# Optimized build, if you need something smaller...\n" +
+		"FROM golang:1.25-alpine AS build\n\n" +
+		"WORKDIR /cmd\n\n" +
 		"COPY go.mod go.sum ./\n" +
 		"RUN go mod download\n\n" +
-		"COPY . .\n" +
-		"RUN go build -v -o /usr/local/bin/app .\n\n" +
-		"CMD [\"app\"]\n",
+		"COPY . .\n\n" +
+		"RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -o /app .\n\n" +
+		"# SWEET LITTLE BINARY\n" +
+		"FROM gcr.io/distroless/static-debian12\n\n" +
+		"WORKDIR /\n\n" +
+		"COPY --from=build /app /app\n\n" +
+		"USER nonroot:nonroot\n\n" +
+		"ENTRYPOINT [\"/app\"]\n",
 )
-
-// TODO: Test this.
-// var DockerFile = []byte(
-// 	"# Optimized build, if you need something smaller...\n" +
-// 		"# Look into golang:1.25-alpine\n" +
-// 		"FROM golang:1.25 AS build\n\n" +
-// 		"WORKDIR /app\n\n" +
-// 		"COPY go.mod go.sum ./\n" +
-// 		"RUN go mod download\n\n" +
-// 		"COPY . .\n" +
-// 		"RUN go build -v -o /app .\n\n" +
-// 		"# SWEET LITTLE BINARY\n" +
-// 		"FROM grc.io/distroless/base-debian10\n\n" +
-// 		"WORKDIR /\n\n" +
-// 		"COPY --from=build /app /app\n\n" +
-// 		"USER nonroot:nonroot\n\n" +
-// 		"ENTRYPOINT [\"/app\"]\n",
-// )
